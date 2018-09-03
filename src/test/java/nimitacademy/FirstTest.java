8bedda5c7d3c2869ed262e4d5b33af6cbaec8f00
Nimit <chavdanimit@gmail.com>
2018-09-03 13:39:23 -0400

    aaabbbcccc

---
+++ src/test/java/nimitacademy/FirstTest.java
@@ -0,0 +1,16 @@
+package nimitacademy;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+	
+@Test	
+public void first(){
+	System.out.println("first hi");
+}
+	
+@Test	
+public void seocnd(){
+	System.out.println("second hi");
+}
+}
---
+++ src/test/java/nimitacademy/SecondTest.java
@@ -0,0 +1,18 @@
+package nimitacademy;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@Test	
+	public void third(){
+		System.out.println("first 3333333333hi");
+	}
+		
+	@Test	
+	public void fourth(){
+		System.out.println("second 444444444hi");
+	}
+	
+	
+}
