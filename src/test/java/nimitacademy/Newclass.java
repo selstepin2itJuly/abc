835c7519a3424783204be465f5a072e27928851e
Nimit <chavdanimit@gmail.com>
2018-09-03 17:06:59 -0400

    newly updated file

---
+++ src/test/java/nimitacademy/Newclass.java
@@ -0,0 +1,19 @@
+package nimitacademy;
+
+import org.testng.annotations.Test;
+
+public class Newclass {
+
+	@Test
+	public void newww(){
+		
+		System.out.println("i am newwwwwwwwwwwww");
+	}
+	
+public void newww2(){
+		
+		System.out.println("i am newwwwwwwwwwwww222222222");
+	}
+	
+	
+}
